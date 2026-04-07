f93f3cd6974ec8aa7e5eeaa1bbf4575dbcbb972a
Israt Jahan Sukonna <ijshoknna2005@gmail.com>
2026-04-07 23:00:23 +0600

    Add files via upload

---
+++ 01_variable/variable.js
@@ -0,0 +1,50 @@
+fullName="Tony Stark";
+
+price=99.99;
+radius=14;
+x=null;
+y=undefined;
+isFollow=false;
+
+
+fullName=25;
+FULLNAME="ISRAT";
+_fullName="Ayesha";
+$fullName="Lakhi";
+console.log(fullName);
+console.log(FULLNAME)
+
+let name="tony"
+console.log(name);
+
+let age=89;
+ age=25;
+ age=56;
+console.log(age);
+
+
+const Age=29;
+console.log(Age);
+
+
+const PI=3.14;
+console.log(PI);
+
+let a;
+console.log(a);      
+
+const a1 = 10;
+console.log(a1);     
+
+{
+    let a = 5;
+    console.log(a);  
+}
+
+{
+    let a=58;
+    console.log(a);
+}
+
+
+
