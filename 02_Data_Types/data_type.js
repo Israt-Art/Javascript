5548c5a453237b93ec1c6f712685945f9633ded8
Israt Jahan Sukonna <ijshoknna2005@gmail.com>
2026-04-09 20:42:59 +0600

    Add files via upload

---
+++ 02_Data_Types/data_type.js
@@ -0,0 +1,29 @@
+let age=24;
+let price=100.5;
+let fullName="Tony";
+isFollow=true;
+
+
+let x=null;
+let x1=BigInt("123");
+
+let y=Symbol("hello");
+
+const student={
+    fullName:"Rahul Kumar",
+    age:50,
+    cgpa:8.2,
+    isPass:true,
+};
+
+student ["age"]=student["age"]+1;
+console.log(student["age"]);
+console.log(student.cgpa);
+
+
+student ["fullName"]="Rahul Sharma";
+console.log(student.fullName);
+
+
+
+
